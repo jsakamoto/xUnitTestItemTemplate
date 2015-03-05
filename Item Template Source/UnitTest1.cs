5cbe36dee8da5524fa95dbbfdbd7fd7646ba94e7
jsakamoto <junichi.jp@gmail.com>
2015-03-05 10:16:08 +0900

    1st commit.

---
+++ Item Template Source/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
